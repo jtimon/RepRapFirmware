faba1c8d492033a37518e90bd59c3c605a53f8c1
Andy Shaw <andy-git@gloomy-place.com>
2023-01-09 15:23:08 +0000

    Fix SBC for new SPI

--- src/Hardware/STM32/Sbc/DataTransfer.hpp
+++ src/Hardware/STM32/Sbc/DataTransfer.hpp
@@ -59,6 +59,6 @@ void setup_spi(void *inBuffer, const void *outBuffer, size_t bytesToTransfer)
 void InitSpi() noexcept
 {
     spiDevice = (HardwareSPI *) SPI::getSSPDevice(SbcSpiChannel);
-    spiDevice->configureDevice(SPI_MODE_SLAVE, 8, (uint8_t)SPI_MODE_0, 100000000, false);
+    spiDevice->configureDevice(SPI_MODE_SLAVE, 8, (uint8_t)SPI_MODE_0, 100000000);
     status = Ready;
 }
